54f8bc358b8e557a378c1a4a54789563c02fbcf8
mendicm <mendicm@gmail.com>
2016-12-19 12:53:32 +0100

    Update PurchaseResponse.php

--- src/Message/PurchaseResponse.php
+++ src/Message/PurchaseResponse.php
@@ -1,7 +1,12 @@
 <?php
+
 namespace Omnipay\Cofidis\Message;
+
 use Omnipay\Common\Message\AbstractResponse;
 use Omnipay\Common\Message\RedirectResponseInterface;
+use Omnipay\Common\Exception\RuntimeException;
+use Symfony\Component\HttpFoundation\RedirectResponse as HttpRedirectResponse;
+use Symfony\Component\HttpFoundation\Response as HttpResponse;
 
 /**
  * Cofidis Purchase Response
@@ -30,5 +35,32 @@ class PurchaseResponse extends AbstractResponse implements RedirectResponseInter
     public function getRedirectData()
     {
         return $this->data;
+    }
+    
+    /**
+     * Overrides original getRedirectResponse() to customize redirect form
+     * @return HttpRedirectResponse | HttpResponse
+     * @throws RuntimeException
+     */
+    public function getRedirectResponse()
+    {
+        if (!$this instanceof RedirectResponseInterface || !$this->isRedirect()) {
+            throw new RuntimeException('This response does not support redirection.');
+        }
+        if ('GET' === $this->getRedirectMethod()) {
+            return HttpRedirectResponse::create($this->getRedirectUrl());
+        } elseif ('POST' === $this->getRedirectMethod()) {
+            $hiddenFields = '';
+            foreach ($this->getRedirectData() as $key => $value) {
+                $hiddenFields .= sprintf(
+                    '<input type="hidden" name="%1$s" value="%2$s" />',
+                    htmlentities($key, ENT_QUOTES, 'UTF-8', false),
+                    htmlentities($value, ENT_QUOTES, 'UTF-8', false)
+                ) . "\n";
+            }
+            $view = \View::make('omnipay.cofidis', ['url' => $this->getRedirectUrl(), 'fields' => $hiddenFields]);
+            return HttpResponse::create($view->render());
+        }
+        throw new RuntimeException('Invalid redirect method "'.$this->getRedirectMethod().'".');
     }
 }
